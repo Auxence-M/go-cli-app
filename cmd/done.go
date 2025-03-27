8251befd292e3b0ce0e1de25a9edb04a1546234e
Auxence-M <auxencemedja@gmail.com>
2025-03-26 22:48:33 -0400

    Added done command

---
+++ cmd/done.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"go-cli-app/todo"
+	"log"
+	"sort"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+var doneCmd = &cobra.Command{
+	Use: "done",
+	Aliases: []string{"do"},
+	Short: "set todo items to done",
+	Long: `done will set a todo item on a todo list to done.
+	The label of a todo item is its position ranging from 1 to n`,
+	Run: doneRun,
+}
+
+func doneRun (cmd *cobra.Command, args []string) {
+	items, err := todo.ReadItems(dataFile)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	i, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatalln(args[0], "is not a valid label\n", err)
+	}
+
+	if i > 0  && i <= len(items) {
+		items[i-1].Done = true
+		fmt.Printf("%q %v\n", items[i-1].Text, "marked done")
+
+		sort.Sort(todo.ByPriority(items))
+		todo.SaveItems(dataFile, items)
+	} else {
+		log.Println(i, "does not match any items")
+	}
+
+}
+func init() {
+	rootCmd.AddCommand(doneCmd)
+}
